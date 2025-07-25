da0f8dca0b48fb2451141768f09c13a01836ff5c
RafaelMenchaca <arafael.menchaca@gmail.com>
2025-07-24 20:42:31 -0500

    cooments added

--- week02-networking/day01_network_check/network_check.sh
+++ week02-networking/day01_network_check/network_check.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+# Display the IP address information of the system
 echo "📡 IP Address Info:"
 ip a
 
 echo ""
+
+# Perform a ping test to check connectivity to google.com
 echo "🌐 Ping Test:"
 ping -c 4 google.com
 
 echo ""
+
+# Perform a traceroute to google.com to analyze the path taken by packets
 echo "🌍 Traceroute:"
 traceroute google.com
